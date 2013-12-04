2fd819c8c096d6e27d9f038a4d6e555c8723f52a
Wei Mingzhi <weimingzhi@gmail.com>
2013-12-04 17:03:23 +0800

    start

---
+++ Android.mk
@@ -0,0 +1,16 @@
+# libiconv
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE	:= iconv
+LOCAL_MODULE_FILENAME := libiconv
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include $(LOCAL_PATH)/libiconv
+
+LOCAL_CFLAGS	:= -Wno-multichar -D_ANDROID -DNO_XMALLOC -DLIBDIR "$(LOCAL_PATH)/libcharset/lib" -DBUILDING_LIBICONV -DIN_LIBRARY -I$(LOCAL_PATH)/include/ -I$(LOCAL_PATH)/lib/ -I$(LOCAL_PATH)/ -I$(LOCAL_PATH)/libcharset/include -w
+
+LOCAL_SRC_FILES := libcharset/lib/localcharset.c lib/relocatable.c lib/iconv.c
+LOCAL_STATIC_LIBRARIES := android-wchar
+
+include $(BUILD_SHARED_LIBRARY)
