9a6575eeacdfcb9630cb3977e1d0de266c26d8fa
Wei Mingzhi <weimzh@neusoft.com>
2013-12-16 09:51:14 +0800

    Use android-support instead

--- Android.mk
+++ Android.mk
@@ -11,6 +11,6 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/include $(LOCAL_PATH)/libiconv
 LOCAL_CFLAGS	:= -Wno-multichar -D_ANDROID -DNO_XMALLOC -DLIBDIR "$(LOCAL_PATH)/libcharset/lib" -DBUILDING_LIBICONV -DIN_LIBRARY -I$(LOCAL_PATH)/include/ -I$(LOCAL_PATH)/lib/ -I$(LOCAL_PATH)/ -I$(LOCAL_PATH)/libcharset/include -w
 
 LOCAL_SRC_FILES := libcharset/lib/localcharset.c lib/relocatable.c lib/iconv.c
-LOCAL_STATIC_LIBRARIES := android-wchar
+LOCAL_STATIC_LIBRARIES := android_support
 
 include $(BUILD_SHARED_LIBRARY)
